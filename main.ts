fa38ae923dc44ee44356d3ab19ede23e98da0209
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2021-11-05 14:12:29 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
+let tenperatura = 0
+music.setVolume(255)
 basic.forever(function () {
-    basic.showLeds(`
-        # . # . #
-        . # # # .
-        # # # # #
-        . # # # .
-        # . # . #
-        `)
+    tenperatura = input.temperature()
+    basic.showNumber(input.temperature())
+    if (tenperatura < 17) {
+        basic.showIcon(IconNames.Chessboard)
+    } else if (tenperatura > 20) {
+        basic.showIcon(IconNames.Square)
+    }
+})
+basic.forever(function () {
+    if (tenperatura < 17) {
+        music.playTone(392, music.beat(BeatFraction.Quarter))
+        music.playTone(330, music.beat(BeatFraction.Quarter))
+    } else if (tenperatura > 20) {
+        music.playTone(370, music.beat(BeatFraction.Quarter))
+        music.playTone(311, music.beat(BeatFraction.Quarter))
+    }
 })
